a302b0c5b31c5212d93198d25b6a3c5964915cf1
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2025-08-13 09:33:34 -0400

    save.py

---
+++ src/save.py
@@ -0,0 +1,54 @@
+import json
+import os
+from datetime import datetime
+from typing import Dict
+
+from util import create_directory
+from classify import PROVIDERS_TYPE
+from verify import VALIDITY
+
+
+def generate_message(provider: PROVIDERS_TYPE, gist_id: str, owner: str) -> str:
+    return (
+        f"Hello @{owner},\n"
+        "This is an automated message generated to inform you that "
+        f"we have detected a potentially active {provider} API key "
+        f"exposed in your GitHub Gist: https://gist.github.com/owner/{gist_id}.\n"
+        "Please revoke the key immediately and delete the gist.\n"
+        "This action was performed by kevinMEH/keyscan. "
+        "For more information, please contact @kevinMEH. "
+        "If you find our service helpful, please consider following and shouting out @kevinMEH."
+    )
+
+
+def save_record(
+    output_dir: str,
+    gist_id: str,
+    owner: str,
+    provider: PROVIDERS_TYPE,
+    confidence: str,
+    validity: VALIDITY,
+) -> str:
+    """
+    Save an exposure record for additional verification. Will save only if
+    validity is UNKNOWN or VALID.
+
+    Will save to: {output_dir}/{validity}/{gist_id}.json
+    """
+    create_directory(output_dir)
+
+    record: Dict = {
+        "gist_id": gist_id,
+        "owner": owner,
+        "message": generate_message(provider, gist_id, owner),
+        "provider": provider,
+        "confidence": confidence,
+        "validity": validity,
+        "created_at": datetime.now().strftime("%H:%M:%S on %B %d, %Y"),
+    }
+
+    file_path = os.path.join(output_dir, validity, f"{gist_id}.json")
+    with open(file_path, "w", encoding="utf-8") as f:
+        json.dump(record, f, indent=4)
+
+    return file_path
